79a666c44ee8223045133494ac13d2035394f785
Pedro Trovo <98984115+TrovoPedro@users.noreply.github.com>
2023-05-05 22:37:41 -0300

    Add files via upload

---
+++ verificar.php
@@ -0,0 +1,26 @@
+<?php
+
+  // Lê os usuários do arquivo users.txt
+  $usuarios = file_get_contents('senhas.txt');
+  $usuarios = json_decode($usuarios, true);
+ 
+   // Obtém os dados do formulário
+  $login = $_POST['login'];
+  $senha = $_POST['senha'];
+  
+  // Procura o usuário com o login e senha fornecidos
+  foreach ($usuarios as $usuario) {
+    if ($usuarios['login'] === $login && $usuarios['senha'] === $senha) {
+        $papel = $usuarios['papel'];
+
+  // Redireciona o usuário para a página de sucesso
+  header('Location: ./GET/usuarios.php?login=' . urlencode($login) . '&papel=' . urlencode($papel));
+  exit();
+ }
+}
+
+  // Redireciona o usuário para a página de erro
+  header('Location: erro.php');
+  exit();
+
+?>
